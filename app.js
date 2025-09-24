c1511a24f249775d2912791a702ca8353edcb28f
Brian Crowe <brian.j.crowe@protonmail.com>
2025-09-23 21:53:08 -0400

    add properties to Movie model

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ void (async () => {
 
   try {
     const movie = await Movie.create({
-      title: 'Toy Story'
+      title: 'Toy Story',
+      runtime: 81,
+      releaseDate: '1995-11-22',
+      isAvailableOnVHS: true
     })
 
     const movie2 = await Movie.create({
-      title: 'The Incredibles'
+      title: 'The Incredibles',
+      runtime: 115,
+      releaseDate: '2004-04-14',
+      isAvailableOnVHS: true
     })
   } catch (err) {
     console.error('Error connecting to the database: ', err)
--- db/models/movie.js
+++ db/models/movie.js
@@ -4,7 +4,21 @@ module.exports = (sequelize) => {
   class Movie extends Sequelize.Model {}
   Movie.init(
     {
-      title: Sequelize.STRING
+      // Set custom primary key column
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      // Default length of STRING is 255, but can specify after STRING(500)
+      title: { type: Sequelize.STRING, allowNull: false },
+      runtime: { type: Sequelize.INTEGER, allowNull: false },
+      releaseDate: { type: Sequelize.DATEONLY, allowNull: false },
+      isAvailableOnVHS: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      }
     },
     { sequelize }
   )
