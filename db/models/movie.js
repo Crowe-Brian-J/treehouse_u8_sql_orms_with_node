ce4830d781833c2a4c1919fce6363e2fbbbf8b5f
Brian Crowe <brian.j.crowe@protonmail.com>
2025-09-23 22:01:20 -0400

    add validators to Movie model

--- db/models/movie.js
+++ db/models/movie.js
@@ -11,13 +11,55 @@ module.exports = (sequelize) => {
         autoIncrement: true
       },
       // Default length of STRING is 255, but can specify after STRING(500)
-      title: { type: Sequelize.STRING, allowNull: false },
-      runtime: { type: Sequelize.INTEGER, allowNull: false },
-      releaseDate: { type: Sequelize.DATEONLY, allowNull: false },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            // Custom error message:
+            msg: 'Please provide a value for "title"'
+          },
+          notEmpty: {
+            // Custom error message:
+            msg: 'Please provide a value for "title"'
+          }
+        }
+      },
+      runtime: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'Please provide a value for "runtime"'
+          },
+          min: {
+            args: 1,
+            msg: 'Please provide a value greater than "0" for "runtime"'
+          }
+        }
+      },
+      releaseDate: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'Please provide a value for "releaseDate"'
+          },
+          isAfter: {
+            args: '1895-12-27',
+            msg: 'Please provide a value on or after "1895-12-28" for "releaseDate"'
+          }
+        }
+      },
       isAvailableOnVHS: {
         type: Sequelize.BOOLEAN,
         allowNull: false,
-        defaultValue: false
+        defaultValue: false,
+        validate: {
+          notNull: {
+            msg: 'Please provide a value for "isAvailableOnVHS"'
+          }
+        }
       }
     },
     { sequelize }
